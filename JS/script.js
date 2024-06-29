d85cc30445fb80422b1feabc6eac9db2fe2375a1
marianavarroa <marianavarroabraham@gmail.com>
2024-06-29 17:46:52 -0300

    Vinculaciones

--- JS/script.js
+++ JS/script.js
@@ -1,55 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('productoForm');
-    const tableBody = document.getElementById('usuariosTable').querySelector('tbody');
+    const form = document.getElementById('perfil');
+    // const tableBody = document.getElementById('usuariosTable').querySelector('tbody');
     let isUpdating = false;
 
     //async permite que la función se comporte de manera asíncrona, 
     //puede ejecutar operaciones sin bloquear el hilo principal de ejecucion
     const fetchusuarios = async () => {
         //luego cambiaremos la url por https://<hostdepanywhere>/usuarios
-        const response = await fetch('https://milepeletay13gg.pythonanywhere.com/usuarios');// promesa: esperar a que se complete la solicitud HTTP
+        const response = await fetch('http://127.0.0.1:5000/usuarios');// promesa: esperar a que se complete la solicitud HTTP
         const usuarios = await response.json(); //esperar a que se complete la conversión de la respuesta a JSON
-        tableBody.innerHTML = '';
-        usuarios.forEach(producto => {
+        form.innerHTML = '';
+        usuarios.forEach(usuario => {
             const row = document.createElement('tr');
             row.innerHTML = `
-                <td>${producto.id}</td>
-                <td>${producto.nombre}</td>
-                <td>${producto.cantidad}</td>
-                <td>${producto.precio}</td>
+                <td>${usuario.id}</td>
+                <td>${usuario.nombre_apellido}</td>
+                <td>${usuario.mail}</td>
+                <td>${usuario.tarjeta}</td>
+                <td>${usuario.plan}</td>
                 <td>
-                    <button onclick="editProducto(${producto.id}, '${producto.nombre}', ${producto.cantidad}, ${producto.precio})">Editar</button>
-                    <button onclick="deleteProducto(${producto.id})">Eliminar</button>
+                    <button onclick="editUsuario(${usuario.id}, '${usuario.nombre_apellido}', ${usuario.mail}, ${usuario.tarjeta}, ${usuario.plan})">Editar</button>
+                    <button onclick="deleteUsuario(${usuario.id})">Eliminar</button>
                 </td>
             `;
-            tableBody.appendChild(row);
+            // tableBody.appendChild(row);
         });
     };
 
-    const addProducto = async (producto) => {
-        await fetch('https://milepeletay13gg.pythonanywhere.com/agregar_usuario', {
+    const addUsuario = async (usuario) => {
+        await fetch('http://127.0.0.1:5000/agregar_usuario', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(producto)
+            body: JSON.stringify(usuario)
         });
         fetchusuarios();
     };
 
-    const updateProducto = async (id, producto) => {
-        await fetch(`https://milepeletay13gg.pythonanywhere.com/actualizar_usuario/${id}`, {
+    const updateUsuario = async (id, usuario) => {
+        await fetch(`http://127.0.0.1:5000/actualizar_usuario/${id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(producto)
+            body: JSON.stringify(usuario)
         });
         fetchusuarios();
     };
 
-    const deleteProducto = async (id) => {
-        await fetch(`https://milepeletay13gg.pythonanywhere.com/eliminar_usuario/${id}`, {
+    const deleteUsuario = async (id) => {
+        await fetch(`http://127.0.0.1:5000/eliminar_usuario/${id}`, {
             method: 'DELETE'
         });
         fetchusuarios();
@@ -57,34 +58,36 @@ document.addEventListener('DOMContentLoaded', () => {
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        const id = document.getElementById('productoId').value;
-        const nombre = document.getElementById('nombre').value;
-        const cantidad = document.getElementById('cantidad').value;
-        const precio = document.getElementById('precio').value;
-        const producto = { nombre, cantidad, precio };
+        const id = document.getElementById('usuarioId').value;
+        const nombre_apellido = document.getElementById('nombre_apellido').value;
+        const mail = document.getElementById('mail').value;
+        const tarjeta = document.getElementById('tarjeta').value;
+        const plan = document.getElementById('plan').value;
+        const usuario = { nombre_apellido, mail, tarjeta, plan };
 
         if (isUpdating) {
-            updateProducto(id, producto);
+            updateUsuario(id, usuario);
             isUpdating = false;
         } else {
-            addProducto(producto);
+            addUsuario(usuario);
         }
 
         form.reset();
-        document.getElementById('productoId').value = '';
+        document.getElementById('usuarioId').value = '';
     });
 
-    window.editProducto = (id, nombre, cantidad, precio) => {
-        document.getElementById('productoId').value = id;
-        document.getElementById('nombre').value = nombre;
-        document.getElementById('cantidad').value = cantidad;
-        document.getElementById('precio').value = precio;
+    window.editUsuario = (id, nombre_apellido, mail, tarjeta, plan) => {
+        document.getElementById('usuarioId').value = id;
+        document.getElementById('nombre_apellido').value = nombre_apellido;
+        document.getElementById('mail').value = mail;
+        document.getElementById('tarjeta').value = tarjeta;
+        document.getElementById('plan').value = plan;
         isUpdating = true;
     };
 
-    window.deleteProducto = (id) => {
-        if (confirm('¿Estás seguro de eliminar este producto?')) {
-            deleteProducto(id);
+    window.deleteUsuario = (id) => {
+        if (confirm('¿Estás seguro de que quieres eliminar tu usuario?')) {
+            deleteUsuario(id);
         }
     };
 
--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 CORS(app)
 
 
-
-
-
 #get -> consultar
 @app.route('/usuarios', methods=['GET'])
 def ver_usuarios():
@@ -35,28 +32,6 @@ def ver_usuarios():
 @app.route('/eliminar_usuario/<int:id>', methods=['DELETE'])
 def eliminar_usuario(id):
     db = mysql.connector.connect(
-        host='milepeletay13gg.mysql.pythonanywhere-services.com',
-        user='milepeletay13gg', #mi usuario
-        password='milonka90', #mi contraseña
-        database='milepeletay13gg$comercio' #nombre de la base de datos
-    )
-
-    cursor = db.cursor()
-    cursor.execute("DELETE FROM usuarios WHERE id = %s", (id,))
-
-    db.commit()
-    cursor.close()
-    return jsonify({"mensaje":"REGISTRO ELIMINADO CON EXITO!!!"})
-
-
-#post -> crear un nuevo elemento en el servidor
-@app.route('/agregar_usuario', methods=['POST'])
-def crear_usuario():
-    info = request.json
-    
-    #info = { "nombre_apellido": "Maria Juarez", "mail": "mj@hotmail.com" , "tarjeta":1005000843664758 , "plan": "Plan Basico" }
-
-    db = mysql.connector.connect(
         host='localhost',
         user='root', #mi usuario
         password='240315Mora', #mi contraseña
@@ -64,11 +39,33 @@ def crear_usuario():
     )
 
     cursor = db.cursor()
-    cursor.execute("INSERT INTO usuarios(nombre_apellido,mail,tarjeta,plan) VALUES(%s,%s,%s,%s)", (info["nombre_apellido"],info["mail"],info["tarjeta"],info["plan"])) #("monitor", 45 , 100500)
+    cursor.execute("DELETE FROM usuarios WHERE id = %s", (id,))
 
     db.commit()
     cursor.close()
-    return jsonify({"mensaje":"USUARIO CREADO CON EXITO!!!"})
+    return jsonify({"mensaje":"USUARIO ELIMINADO CON EXITO!!!"})
+
+
+#post -> crear un nuevo elemento en el servidor
+@app.route('/agregar_usuario', methods=['POST'])
+def crear_usuario():
+     info = request.json
+    
+     #info = { "nombre_apellido": "Maria Juarez", "mail": "mj@hotmail.com" , "tarjeta":1005000843664758 , "plan": "Plan Basico" }
+
+     db = mysql.connector.connect(
+         host='localhost',
+         user='root', #mi usuario
+         password='240315Mora', #mi contraseña
+         database='suscripciones' #nombre de la base de datos
+     )
+
+     cursor = db.cursor()
+     cursor.execute("INSERT INTO usuarios(nombre_apellido,mail,tarjeta,plan) VALUES(%s,%s,%s,%s)", (info["nombre_apellido"],info["mail"],info["tarjeta"],info["plan"])) #("monitor", 45 , 100500)
+
+     db.commit()
+     cursor.close()
+     return jsonify({"mensaje":"USUARIO CREADO CON EXITO!!!"})
 
 
 
@@ -80,18 +77,18 @@ def modificar_usuario(id):
     #info = { "nombre": "monitor", "categoria": 45 , "precio":100500}
     
     db = mysql.connector.connect(
-        host='milepeletay13gg.mysql.pythonanywhere-services.com',
-        user='milepeletay13gg', #mi usuario
-        password='milonka90', #mi contraseña
-        database='milepeletay13gg$comercio' #nombre de la base de datos
+        host='localhost',
+        user='root', #mi usuario
+        password='240315Mora', #mi contraseña
+        database='suscripciones' #nombre de la base de datos
     )
 
     cursor = db.cursor()
-    cursor.execute("UPDATE usuarios SET nombre= %s, cantidad= %s, precio= %s WHERE id = %s", (info["nombre"],info["cantidad"],info["precio"] , id)) #("monitor", 45 , 100500)
+    cursor.execute("UPDATE usuarios SET nombre_apellido= %s, mail= %s, tarjeta= %s, plan= %s WHERE id = %s", (info["nombre_apellido"],info["mail"],info["tarjeta"],info["plan"], id)) #("monitor", 45 , 100500)
 
     db.commit()
     cursor.close()
-    return jsonify({"mensaje":"REGISTRO ACTUALIZADO CON EXITO!!!"})
+    return jsonify({"mensaje":"USUARIO ACTUALIZADO CON EXITO!!!"})
 
 
 if __name__ == '__main__':
@@ -99,19 +96,19 @@ if __name__ == '__main__':
 
 
 
-# from flask import Flask, jsonify
-# import mysql.connector
-# from flask_cors import CORS 
+from flask import Flask, jsonify
+import mysql.connector
+from flask_cors import CORS 
 
-# app = Flask(__name__)
-# CORS(app)
+app = Flask(__name__)
+CORS(app)
 
-#get -> consultar
-#post -> crear un nuevo elemento en el servidor
-#delete -> eliminar
-#put -> actualizar
+# #get -> consultar
+# #post -> crear un nuevo elemento en el servidor
+# #delete -> eliminar
+# #put -> actualizar
 
- #consultar
+# #consultar
 @app.route('/usuarios', methods=['GET'])
 def ver_usuarios():
      db = mysql.connector.connect(
